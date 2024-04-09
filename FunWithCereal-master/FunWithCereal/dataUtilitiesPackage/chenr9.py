d64a93683f2edcfb5649c7511ffc7c28a7a9298b
zihao <zihao@10.11.157.106>
2024-04-09 13:11:54 -0400

    Ruolin Chen, chenr9

---
+++ FunWithCereal-master/FunWithCereal/dataUtilitiesPackage/chenr9.py
@@ -0,0 +1,46 @@
+#Name:Ruolin Chen
+#email:chenr9@mail.uc.edu
+#Assignment Title: Assignment 10
+#Course: IS 4010
+#Semester/Year: Spring 2024
+#Brief Description: This module contains a function that generates an interesting about the cereal data
+#Citations:
+#Anything else that's relevant: 
+
+
+#from csvUtilitiesPackage.csvUtilities import *
+import csv
+
+def readCSV():
+    """
+    Read the cereals CSV file into a list of lists
+    @return: The list of lists
+    """
+    with open('..\dataPackage\\cereals.txt') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='\t')
+        line_count = 0
+        cereals = []
+        for row in csv_reader:
+            if line_count == 0:
+                #print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                cereals.append(row)
+                #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+                line_count += 1
+        #print(cereals)
+    return cereals
+
+def chenr9():
+    '''
+    this function calls readCSV and prints out a fun fact about my cereal
+    @sparam none:
+    '''
+    
+    cerealInfo = readCSV()
+    print("My favorite cereal type is:", cerealInfo[0][0], "Puffed Wheat.")
+
+
+if __name__ == "__main__":
+    chenr9()
+   
