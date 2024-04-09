1245ce6b7f85555fc5d6d651acc69af0654d7d6e
aimeemadrigal <aimeemadrigal@localhost>
2024-04-09 13:20:11 -0400

    Merge branch 'master' of https://github.com/aimeecatherine14/FunWithCereal-001

--- FunWithCereal-master/FunWithCereal/dataUtilitiesPackage/ignaciac.py
+++ FunWithCereal-master/FunWithCereal/dataUtilitiesPackage/ignaciac.py
@@ -13,8 +13,31 @@
 
 
 
+# from csvUtilitiesPackage.csvUtilities import *
+import csv 
 
-from csvUtilitiesPackage.csvUtilities import *
+
+# I did this because I'm on a Mac.
+
+def readCSV():
+    """
+    Read the cereals CSV file into a list of lists
+    @return: The list of lists
+    """
+    with open('..\dataPackage\\cereals.txt') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='\t')
+        line_count = 0
+        cereals = []
+        for row in csv_reader:
+            if line_count == 0:
+                #print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                cereals.append(row)
+                #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+                line_count += 1
+        #print(cereals)
+    return cereals
 
 def ignaciac():
     '''
