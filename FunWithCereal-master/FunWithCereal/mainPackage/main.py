bd78d18ef8cbe7f8994eba9269f75fb3fa5ce74f
nicomp42 <nicholdw@ucmail.uc.edu>
2024-04-09 10:37:38 -0400

    Added support for all student IDs

--- FunWithCereal-master/FunWithCereal/mainPackage/main.py
+++ FunWithCereal-master/FunWithCereal/mainPackage/main.py
@@ -3,24 +3,20 @@
 # Bill Nicholson
 # nicholdw@ucmail.uc.edu
 # IS4010-001 Spring 2024
-
-from csvUtilitiesPackage.csvUtilities import *
+from FunWithCereal.csvUtilitiesPackage.csvUtilities import *
+#from csvUtilitiesPackage.csvUtilities import *
 
 if __name__ == "__main__":
-
-    students = ["cunninig", "gilligtp", "harrints", "jarrelbc"
-              , "kleinbw",  "lisowsmd", "moorehc",  "patel5a5"
-              , "shahh4",   "stapleet", "troegele", "wilkinrs"
-              , "willi6d3", "wogomomr"]
+    students = readStudentIDs()
     for student in students:
         try:
             # Build the import statement in a string and execute it
-            cmd = "from dataUtilitiesPackage." + student + " import " + student
+            cmd = "from FunWithCereal.dataUtilitiesPackage." + student + " import " + student
             exec(cmd)
         except Exception as e:
             print(e) 
-            print("cmd:", cmd)
-            print("**** Import failed for " + student + "*****")
+            #print("cmd:", cmd)
+            #print("**** Import failed for " + student + "*****")
 
     myCereals = readCSV()
     print(myCereals)
